b9bdc16bb3d0e2941111abc6c65c7f40fa62d749
Johnny Cassidy <developer@ioconic.dev>
2023-12-01 19:45:14 +0000

    Add initial solution

--- day01/solution.js
+++ day01/solution.js
@@ -2,4 +2,33 @@ import readInput from "../read-input.js";
 
 const input = readInput();
 
-console.log(input);
+const solution = splitInput => {
+  let firstNumber, lastNumber, finalNumber, result = 0;
+
+  splitInput.forEach(inputString => {
+    const splitInputString = inputString.trim().split('');
+    const reversedSplitInputString = [...splitInputString].reverse();
+
+    for (const char of splitInputString) {
+      if (Number(char)) {
+        firstNumber = char;
+        break;
+      }
+    }
+
+    for (const char of reversedSplitInputString) {
+      if (Number(char)) {
+        lastNumber = char;
+        break;
+      }
+    }
+
+    if (firstNumber) {
+      finalNumber = Number(`${firstNumber}${lastNumber}`);
+      result += finalNumber;
+    }
+  });
+  return result;
+}
+
+console.log(solution(input));
