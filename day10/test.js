e13e6f744c70d593c7ea16e9d963e9781fd918f1
Jeo D <jeo@dfor.dev>
2023-12-10 11:56:37 -0700

    Add day 10 skeleton

---
+++ day10/test.js
@@ -0,0 +1,30 @@
+import { describe, it } from 'node:test';
+import assert from 'node:assert';
+import readInput from '../read-input.js';
+import { part1, part2 } from './solution.js';
+
+describe('day10 solution', () => {
+  describe('part 1', () => {
+    it('should return the correct value using the example input 1', () => {
+      const input = readInput('day10/example-input-1');
+      const result = part1(input);
+      assert.strictEqual(result, 4);
+    });
+
+    it('should return the correct value using the example input 2', () => {
+      const input = readInput('day10/example-input-2');
+      const result = part1(input);
+      assert.strictEqual(result, 8);
+    });
+
+    // it('should return the correct value using the puzzle input', () => {
+    //   const input = readInput('day10/input');
+    //   const result = part1(input);
+    //   assert.strictEqual(result, ?);
+    // });
+  });
+
+  describe('part 2', () => {
+
+  });
+});
