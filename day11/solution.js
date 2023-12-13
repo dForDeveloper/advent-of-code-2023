d3fff466e308bd4b5d1743b3e07b8ddfd61c961b
Jeo D <jeo@dfor.dev>
2023-12-12 19:54:27 -0700

    Complete part 2 of day 11

--- day11/solution.js
+++ day11/solution.js
@@ -1,62 +1,78 @@
 import readInput from '../read-input.js';
 
-const expandInput = (input) => {
+const findEmptyRowsAndColumns = (input) => {
   const emptyRowIndexes = new Set();
-  const columnsWithHashes = new Set();
+  const emptyColumnIndexes = new Set();
+
+  for (let i = 0; i < input[0].length; i++) {
+    emptyColumnIndexes.add(i);
+  }
 
   input.forEach((line, i) => {
     const matches = Array.from(line.matchAll(/#/g));
     if (!matches.length) {
       emptyRowIndexes.add(i);
     } else {
-      matches.forEach(match => columnsWithHashes.add(match.index));
+      matches.forEach(match => emptyColumnIndexes.delete(match.index));
     }
   });
 
-  return input.reduce((expandedInput, line, i) => {
-    const expandedLine = line.split('').reduce((newLine, char, j) => {
-      newLine += char;
-      if (!columnsWithHashes.has(j)) {
-        newLine += '.';
-      }
-      return newLine;
-    }, '');
-
-    expandedInput.push(expandedLine);
-    if (emptyRowIndexes.has(i)) {
-      expandedInput.push(expandedLine);
-    }
-
-    return expandedInput;
-  }, []);
+  return [emptyRowIndexes, emptyColumnIndexes];
 };
 
 const locateHashes = (input) => {
-  return input.reduce((locations, line, x) => {
+  return input.reduce((locations, line, y) => {
     const hashMatches = Array.from(line.matchAll(/#/g));
-    return [...locations, ...hashMatches.map(match => ({ x, y: match.index }))];
+    return [...locations, ...hashMatches.map(match => ({ y, x: match.index }))];
   }, []);
 };
 
-const calculateDistances = (locations) => {
+const calculateOneDimensionalDistanceWithExpandedEmptySpace = (a, b, emptySpaces, expansionFactor) => {
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+  let expandedSpace = max - min;
+  for (let i = min; i < max; i++) {
+    if (emptySpaces.has(i)) {
+      expandedSpace += expansionFactor;
+    }
+  }
+  return expandedSpace;
+};
+
+const calculateDistances = (locations, emptyRows, emptyColumns, expansionFactor) => {
   const distances = [];
   for (let i = 0; i < locations.length - 1; i++) {
     for (let j = i + 1; j < locations.length; j++) {
-      distances.push(Math.abs(locations[i].x - locations[j].x) + Math.abs(locations[i].y - locations[j].y));
+      let horizontalDistance = calculateOneDimensionalDistanceWithExpandedEmptySpace(
+        locations[i].x,
+        locations[j].x,
+        emptyColumns,
+        expansionFactor
+      );
+      let verticalDistance = calculateOneDimensionalDistanceWithExpandedEmptySpace(
+        locations[i].y,
+        locations[j].y,
+        emptyRows,
+        expansionFactor
+      );
+      distances.push(horizontalDistance + verticalDistance);
     }
   }
   return distances;
 };
 
 const part1 = (input) => {
-  const expandedInput = expandInput(input);
-  const hashLocations = locateHashes(expandedInput);
-  const distances = calculateDistances(hashLocations);
+  const [emptyRowIndexes, emptyColumnIndexes] = findEmptyRowsAndColumns(input);
+  const hashLocations = locateHashes(input);
+  const distances = calculateDistances(hashLocations, emptyRowIndexes, emptyColumnIndexes, 1);
   return distances.reduce((sum, num) => sum + num, 0);
 };
 
 const part2 = (input) => {
-
+  const [emptyRowIndexes, emptyColumnIndexes] = findEmptyRowsAndColumns(input);
+  const hashLocations = locateHashes(input);
+  const distances = calculateDistances(hashLocations, emptyRowIndexes, emptyColumnIndexes, 999999);
+  return distances.reduce((sum, num) => sum + num, 0);
 };
 
 if (process.env.NODE_ENV !== 'test') {
--- day11/test.js
+++ day11/test.js
@@ -1,7 +1,7 @@
 import { describe, it } from 'node:test';
 import assert from 'node:assert';
 import readInput from '../read-input.js';
-import { part1 } from './solution.js';
+import { part1, part2 } from './solution.js';
 
 describe('day11 solution', () => {
   describe('part 1', () => {
@@ -15,6 +15,20 @@ describe('day11 solution', () => {
       const input = readInput('day11/input');
       const result = part1(input);
       assert.strictEqual(result, 9623138);
+    });
+  });
+
+  describe('part 2', () => {
+    it('should return the correct value using the example input', () => {
+      const input = readInput('day11/example-input-1');
+      const result = part2(input);
+      assert.strictEqual(result, 82000210);
+    });
+
+    it('should return the correct value using the puzzle input', () => {
+      const input = readInput('day11/input');
+      const result = part2(input);
+      assert.strictEqual(result, 726820169514);
     });
   });
 });
